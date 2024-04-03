57f518fe966731603e125dfdc66419736dede726
AbidiM <mohamed-abidi@live.fr>
2024-04-03 10:16:43 +0100

    initial commit 1016

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json({limit: '20mb'}));
+app.use(express.urlencoded({ extended: false, limit: '20mb' }));
+
 const PORT = process.env.PORT || 3000;
 const writeRead = require('./routes/writeRead');
 const updateDelete = require('./routes/updateDelete');
+
 app.use('/cr', writeRead);
 app.use('/ud', updateDelete);
 app.use('/', function (req, res, next) {
