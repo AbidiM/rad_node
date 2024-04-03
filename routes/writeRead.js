0bd4a33f2e0f9a95cc34cecba3db91ba938895aa
AbidiM <mohamed-abidi@live.fr>
2024-04-03 09:38:39 +0100

    initial commit 5

--- routes/writeRead.js
+++ routes/writeRead.js
@@ -1,31 +1,30 @@
 'use strict';
 const express = require('express');
 const crRoute = express.Router();
+
 const connection = require('../db');
+
+
 crRoute.post('/signup', function (req, res, next) {
-console.log("this is the signup crRoute");
 
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-
-    console.log(name + " + " + email + " + " + password);
+    console.log(req.params.name + " + " + req.params.email + " + " + req.params.password);
 
     connection.execute(`INSERT INTO users 
     (username, email, password) 
     VALUES (?, ?, ?);`,
-        [name, email, password])
+        [req.params.name, req.params.email, req.params.password])
         .then(() => {
             console.log('ok');
         }).catch((err) => {
-            
-            console.log('error asba');
             console.log(err);
         });
     res.end();
 });
+
 crRoute.get('/login', function (req, res, next) {
-    connection.execute('SELECT * FROM users WHERE email = ? AND password = ?;', [email, password])
+
+    console.log(req.body.email + " + " + req.body.password);
+    connection.execute('SELECT * FROM users WHERE email = ? AND password = ?;', [req.params.email, req.params.password])
         .then((result) => {
             var rawData = result[0];
             console.log(rawData);
@@ -36,8 +35,10 @@ crRoute.get('/login', function (req, res, next) {
         });
 });
 crRoute.post('/check', function (req, res, next) {
+
+    console.log(req.params.email);
     connection.execute('SELECT * FROM users WHERE email=?;',
-        [req.body.email])
+        [req.params.email])
         .then((result) => {
             var data = result[0];
             if (data.length === 0) {
@@ -50,6 +51,7 @@ crRoute.post('/check', function (req, res, next) {
             res.sendStatus(404);
         });
 });
+
 crRoute.use('/', function (req, res, next) {
     res.sendStatus(404);
 })
--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const writeRead = require('./routes/writeRead');
 const updateDelete = require('./routes/updateDelete');
-const udRoute = require('./routes/updateDelete');
 app.use('/cr', writeRead);
 app.use('/ud', updateDelete);
 app.use('/', function (req, res, next) {
-    console.log('this api is working');
     res.sendStatus(404);
 });
 
+
 app.listen(PORT, () =>
-    console.log('this Server running on port: ' + PORT
+    console.log('Server running on port: ' + PORT
     ));
